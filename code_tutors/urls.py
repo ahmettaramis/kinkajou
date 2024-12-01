f76c38fa2c56171b6effe4c66faccdcd95c921cb
SihasA <sabeywickra@gmail.com>
2024-12-01 11:30:11 +0000

    fix: Correct URL name inconsistency in lesson request redirection

--- code_tutors/urls.py
+++ code_tutors/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
      # Student views
     path('lesson_requests/create/', views.create_lesson_request, name='create_lesson_request'),
-    path('lesson_requests/view/', views.student_view_requests, name='student_view_requests'),
+    path('lesson_requests/view/', views.student_view_requests, name='student_requests'),
     # Admin views
     path('lesson_requests/admin/', views.admin_view_requests, name='admin_view_requests'),
     path('lesson_requests/<int:pk>/update-status/', views.update_request_status, name='update_request_status'),
