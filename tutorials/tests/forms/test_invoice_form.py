ac8d398c82c14ac19cef5a2c0566d7459382e1a5
Raphael Ahiable <raphahi@hotmail.com>
2024-12-11 17:09:45 +0000

    Update invoice test classes Fix: Remove redundant invoices.html

--- tutorials/tests/forms/test_invoice_form.py
+++ tutorials/tests/forms/test_invoice_form.py
@@ -3,7 +3,7 @@ from django import forms
 from django.test import TestCase
 from django.utils import timezone
 from tutorials.forms import InvoiceForm
-from tutorials.models import User, Invoice, Lesson
+from tutorials.models import User, Invoice, LessonRequest
 from decimal import Decimal
 import datetime
 
@@ -21,18 +21,24 @@ class InvoiceFormTestCase(TestCase):
         student = User.objects.get(username='@charlie')
         tutor = User.objects.get(username='@janedoe')
 
-        self.lesson = Lesson.objects.create(tutor=tutor, student=student, description='test description.', time=self.now)
-
+        self.lesson_request = LessonRequest.objects.create(
+            student=student, 
+            title="Test Title.", 
+            description="Test Description.", 
+            status="unallocated", 
+            lesson_date = timezone.now(), 
+            preferred_tutor=tutor, no_of_weeks=12
+            )
+        
         self.form_input = {
-            'lesson' : self.lesson,
+            'lesson_request' : self.lesson_request,
             'amount' : 9999.99,
-            'is_paid' : False,
-            'created_at' : self.now
+            'is_paid' : False
         }
 
     def test_form_has_necessary_fields(self):
         form = InvoiceForm()
-        self.assertIn('lesson', form.fields)
+        self.assertIn('lesson_request', form.fields)
         self.assertIn('amount', form.fields)
         self.assertIn('is_paid', form.fields)
 
@@ -41,13 +47,12 @@ class InvoiceFormTestCase(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_form_must_save_correctly(self):
-        invoice = Invoice.objects.create(lesson=self.lesson, amount=9999.99, is_paid=False, created_at=self.now)
+        invoice = Invoice.objects.create(lesson_request=self.lesson_request, amount=9999.99, is_paid=False)
         form = InvoiceForm(instance=invoice, data=self.form_input)
         before_count = User.objects.count()
         form.save()
         after_count = User.objects.count()
         self.assertEqual(after_count, before_count)
-        self.assertEqual(invoice.lesson, self.lesson)
+        self.assertEqual(invoice.lesson_request, self.lesson_request)
         self.assertEqual(invoice.amount, Decimal('9999.99'))
         self.assertEqual(invoice.is_paid, False)
-        self.assertEqual(invoice.created_at, self.now)
